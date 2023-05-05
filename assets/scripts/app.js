5fd15d0a4a73645d320a20eae3a0dc6ebbd8cbd2
Mohannadgithub <mohannadattal85@gmail.com>
2023-05-05 14:32:07 +0200

    Creating a Movie  & Clearing the Input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,9 @@ const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const addConfirmMovie = cancelAddMovieButton.nextElementSibling;
-const userInput = addMovieModal.querySelectorAll('input');
+const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById(entry - text);
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -20,12 +22,13 @@ const backdropClickHandler = () => {
 
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
-  const titleValue = userInput[0].value;
-  const imageUrlValue = userInput[1].value;
-  const ratingValue = userInput[2].value;
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
   if (
     titleValue.trim === '' ||
     imageUrlValue.trim === '' ||
@@ -36,7 +39,31 @@ const addMovieHandler = () => {
     alert('Please enter valid values(rating between 1 and 5) ');
     return;
   }
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
 };
+
+const clearMovieInput = () => {
+  for (const userInput of userInputs) {
+    userInput.value = '';
+  }
+};
+
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display('block')
+    } else {
+        
+    }
+}
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
